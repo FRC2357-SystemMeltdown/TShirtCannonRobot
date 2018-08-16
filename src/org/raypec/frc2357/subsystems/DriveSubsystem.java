2e6d3e3515f8381d658f716f101f8e5a5cf737f4
coderkevin <kevin@redfenix.com>
2018-08-16 17:10:22 -0500

    Switch to Arcade Drive

--- src/org/raypec/frc2357/subsystems/DriveSubsystem.java
+++ src/org/raypec/frc2357/subsystems/DriveSubsystem.java
@@ -8,30 +8,31 @@ package org.raypec.frc2357.subsystems;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.Talon;
-import org.raypec.frc2357.commands.TankDriveCommand;
+import org.raypec.frc2357.commands.ArcadeDriveCommand;
 
 /**
- *
  * @author Kevin
  */
 public class DriveSubsystem extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 	
-	private Talon left = new Talon(1);
-	private Talon right = new Talon(2);
-	private RobotDrive drive = new RobotDrive( left, right );
+	private final Talon left = new Talon(1);
+	private final Talon right = new Talon(2);
+	private final RobotDrive drive = new RobotDrive( left, right );
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
-		setDefaultCommand(new TankDriveCommand());	
+		setDefaultCommand(new ArcadeDriveCommand());	
 	}
 	
 	public void arcade(double speed, double rotation) {
+		System.out.println( "speed: " + speed + ", rotation: " + rotation );
 		drive.arcadeDrive(speed, rotation);
 	}
 	
 	public void tankDrive(double left, double right) {
+		System.out.println( "left: " + left + ", right: " + right );
 		drive.tankDrive(left, right);
 	}
 }
